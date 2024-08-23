954335243877dc1558384dc1ce58f80e8f775525
Luigi <luigimete54@gmail.com>
2024-08-23 18:26:01 -0300

    Correcciones

--- Trabajos practicos/TpClase/coinApi.js
+++ Trabajos practicos/TpClase/coinApi.js
@@ -1,15 +1,30 @@
-var COIN_API_SDK = (function () {
-    if (apiKey === void 0) {apiKey = null;}
-    this.apiKey = ""
-    this.headers = {}
-    this.url = "https://rest-test.coinapi.io/v1/exchanges"
-    if(apiKey) {
-        this.apiKey = apiKey
-        this.headers = {
-            "X-CoinAPI-key": apiKey
-        }
-        this.url = "https://rest.coinapi.io"
-    }
-})
+require('dotenv').config({path:'..../.env'})
+process.env.api_key;
 
-const apiKey = '51BE0883-2A87-434D-B9C3-FD0862FAED57'
+const apiKey = api_key
+const url = ' https://rest.coinapi.io/v1/exchanges'
+const fetchData = async () => {
+    try {
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'X-CoinAPI-Key': apiKey
+            }
+        })
+
+        if (!response.ok) {
+            throw new Error('Network response was not ok ' + response.statusText)
+        }
+        const data = await response.json()
+        data.forEach(exchange => {
+            console.log('Nombre:', exchange.name)
+            console.log('Volumen en 1 hora:', exchange.volume_1hrs_usd)
+            console.log('Volumen en 1 dia:', exchange.volume_1day_usd)
+            console.log('-----------------------------------')
+        })
+    } catch (error) {
+        console.error('Hubo un problema con el Fetch:', error)
+    }
+}
+
+window.onload = fetchData
